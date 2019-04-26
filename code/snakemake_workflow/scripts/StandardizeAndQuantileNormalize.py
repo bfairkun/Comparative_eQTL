bfc78b0de1c3b6c4954e36e42f3fa293c5c7068f
Benjmain Fair <bfair.kun@gmail.com>
2019-04-26 09:30:09 -0500

    added gene normalization script

--- code/snakemake_workflow/scripts/StandardizeAndQuantileNormalize.py
+++ code/snakemake_workflow/scripts/StandardizeAndQuantileNormalize.py
@@ -2,6 +2,7 @@
 import sys
 import numpy as np
 import pandas as pd
+from scipy.stats import norm
 
 MatrixFileIn, MatrixFileOut = sys.argv[1:]
 
@@ -14,5 +15,18 @@ def quantile_normalize_using_target(x, target):
 
     return target_sorted[x.argsort().argsort()]
 
+#Read file of N rows (genes) and M columns (individuals)
 df=pd.read_csv(MatrixFileIn, index_col=0, sep='\t')
-print(df)
+
+#Standardize across individuals to get Z scores.
+StandardizedMatrix = df.sub(df.mean(1), axis=0).div(df.std(1), axis=0)
+
+#Get N quantiles of normal distribution
+NormalQuantiles = norm.ppf(np.linspace(0,1,df.shape[0]+2))[1:-1]
+
+# Quantile normalize across genes.
+QuantileNormalizedMatrix =  StandardizedMatrix.apply(quantile_normalize_using_target, axis=0, result_type='broadcast', target=NormalQuantiles)
+QuantileNormalizedMatrix.to_csv(MatrixFileOut)
+
+# Write out file
+QuantileNormalizedMatrix.to_csv( MatrixFileOut, sep='\t', index_label="IID")
