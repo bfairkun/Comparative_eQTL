c7a15662add13ef77b13c645df89535f10a3946d
Benjmain Fair <bfair.kun@gmail.com>
2019-04-27 13:35:22 -0500

    fixed Rscript to sum transcript TPMs to gene level TPM

--- analysis/20190327_MakeFamPhenotypeFile.R
+++ analysis/20190327_MakeFamPhenotypeFile.R
@@ -5,10 +5,12 @@ knitr::opts_chunk$set(echo = TRUE)
 ## ----load-libraries, message=F, warning=F--------------------------------
 library(tidyverse)
 library(knitr)
+library(matrixStats)
+
 
 
 ## ------------------------------------------------------------------------
-# knitr::purl(input="../analysis/20190327_MakeFamPhenotypeFile.Rmd", output="../analysis/20190327_MakeFamPhenotypeFile.R")
+knitr::purl(input="../analysis/20190327_MakeFamPhenotypeFile.Rmd", output="../analysis/20190327_MakeFamPhenotypeFile.R")
 
 
 ## ----Set-filepaths-------------------------------------------------------
@@ -20,10 +22,12 @@ if(commandArgs()[4] == "--file=../../analysis/20190327_MakeFamPhenotypeFile.R"){
   PhenotypeOutFilepath <- args[3]
   PhenotypeListOutFilepath <- args[4]
   GenesBedFile <- args[5]
+  TranscriptToGenesFile <- args[6] #Two column mapping of gene_id(col1) and transcript_id(col2)
 } else {
   CountFilepath <- '../output/CountTable.tpm.txt.gz'
   EmptyFamFilepath <- '../output/ForAssociationTesting.temp.fam'
-  GenesBedFile <- '../data/cDNA.all.chromosomal.bed'
+  GenesBedFile <- '../output/Genes.bed'
+  TranscriptToGenesFile <- '../data/Biomart_export.Pan_Tro_3.geneids.txt'
 }
 
 
@@ -43,18 +47,43 @@ GeneChromosomes <- read.table(GenesBedFile, col.names=c("chromosome", "start", "
   select(gene, chromosome)
 kable(head(GeneChromosomes))
 
+# ensembl transcript and gene-ids
+TranscriptsToGenes <- read.table(TranscriptToGenesFile, sep='\t', header=T, stringsAsFactors = F) %>% select(Gene.stable.ID, Transcript.stable.ID)
+kable(head(TranscriptsToGenes))
+
 
 ## ------------------------------------------------------------------------
-GeneSet1 <- CountTable %>%
-  rownames_to_column('gene') %>%
-  mutate(Mean = rowMeans(select(., -gene))) %>%
-  filter(Mean>1) %>%
-  pull(gene)
+#How many genes are there
+length(unique(TranscriptsToGenes$Gene.stable.ID))
 
-GeneSet2 <- CountTable %>%
+#filter count table for autosomal genes, and sum transcript TPM to gene level TPM.
+MyDf <- CountTable %>%
   rownames_to_column('gene') %>%
+  mutate(Transcript.stable.ID=sub(".\\d+$", "", gene)) %>%
+  select(-gene) %>%
+  left_join(TranscriptsToGenes, by="Transcript.stable.ID") %>%
+  select(-Transcript.stable.ID) %>%
+  aggregate(.~Gene.stable.ID, ., sum) %>%
+  left_join(GeneChromosomes, by=c("Gene.stable.ID"="gene")) %>%
+  filter(!chromosome %in% c("X", "Y", "MT")) %>%
+  select(-chromosome)
+
+  
+Median <- MyDf %>%
+  column_to_rownames('Gene.stable.ID') %>%
+  mutate(Median = rowMedians(as.matrix(.))) %>% pull(Median)
+
+# histogram of median expression [ log10(TPM) ] of genes 
+hist(log10(Median))
+
+GeneSet1 <- MyDf %>%
+  mutate(Median=Median) %>%
+  filter(Median>0.1) %>%
+  pull(Gene.stable.ID)
+
+GeneSet2 <- MyDf %>%
   filter_if(is.numeric, all_vars(.>0)) %>%
-  pull(gene)
+  pull(Gene.stable.ID)
 
 #Number genes left after filter method1
 length(GeneSet1)
@@ -68,14 +97,12 @@ length(intersect(GeneSet1, GeneSet2))
 
 
 ## ------------------------------------------------------------------------
-PhenotypesToOutput <- CountTable %>%
-  rownames_to_column('gene') %>%
+
+# Output is log10(TPM)
+PhenotypesToOutput <- MyDf %>%
   filter_if(is.numeric, all_vars(.>0)) %>%
-  merge(GeneChromosomes, by="gene", all.x=T) %>%
-  filter(!chromosome %in% c("X", "Y", "MT")) %>%
-  select(-chromosome) %>%
-  column_to_rownames('gene') %>%
-  log() %>%
+  column_to_rownames('Gene.stable.ID') %>%
+  log10() %>%
   t()
 
 row.names(PhenotypesToOutput) <- colnames(CountTable)
