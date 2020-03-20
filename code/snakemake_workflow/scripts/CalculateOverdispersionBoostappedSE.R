aa8593fee3e7d4d18595fccc1b5063e9d113f053
Benjmain Fair <bfair.kun@gmail.com>
2020-03-20 18:02:13 -0400

    added bootstrap SE script

--- code/snakemake_workflow/scripts/CalculateOverdispersionBoostappedSE.R
+++ code/snakemake_workflow/scripts/CalculateOverdispersionBoostappedSE.R
@@ -1,7 +1,6 @@
-setwd("/Users/benfair/Documents/GiladLiProjects/Repos/Comparative_eQTL/code/snakemake_workflow/")
+# setwd("/Users/benfair/Documents/GiladLiProjects/Repos/Comparative_eQTL/code/snakemake_workflow/")
 
 library(tidyverse)
-library(knitr)
 library("edgeR")
 source("../CustomFunctions.R")
 
@@ -13,31 +12,88 @@ GetLoessResidual <- function(x, y){
   return(loess.residual)
 }
 
+rep.col<-function(x,n){
+  matrix(rep(x,each=n), ncol=n, byrow=TRUE)
+}
+
+Resampled.GetDispersion <- function(CountMatrix, GeneLengthsMatrix, n=NULL, N_genes=NULL, seed=0){
+  set.seed(seed)
+  if(is.null(N_genes)){
+    NumGenes <- nrow(CountMatrix)
+  }
+  else{
+    NumGenes<-N_genes
+  }
+  if(is.null(n)){
+    NumInds <- ncol(CountMatrix)
+  }
+  else{
+    NumInds<-n
+  }
+  ColsSample <- sample(x=ncol(CountMatrix), size=NumInds, replace=T)
+  Estimates <- GetParameterEstimatesOfUnderlyingGamma_lengthAdjusted_FromTable(CountMatrix[1:NumGenes,ColsSample], GeneLengthsMatrix[1:NumGenes,ColsSample])
+  Dispersion <- GetLoessResidual(Estimates$mu, log(Estimates$overdispersion))
+
+  return(setNames(Dispersion, rownames(CountMatrix[1:NumGenes,])))
+}
+
+### Global vars
+CountTableChimpFile <- '../../output/PowerAnalysisFullCountTable.Chimp.subread.txt.gz'
+CountTableHumanFile <- '../../output/PowerAnalysisFullCountTable.Human.subread.txt.gz'
+OutputDE <- '../../output/Final/TableS1.tab'
+OutputDispersionEstimatesChimp <- 'OverdispersionBootsrappedIterations/Chimp.1.2.tab'
+OutputDispersionEstimatesHuman <- 'OverdispersionBootsrappedIterations/Human.1.2.tab'
+InitialSeed <- 1
+Npermutations <- 5
+
+args = commandArgs(trailingOnly=TRUE)
+CountTableChimpFile <- args[1]
+CountTableHumanFile <- args[2]
+OutputDE <- args[3]
+OutputDispersionEstimatesChimp <- args[4]
+OutputDispersionEstimatesHuman <- args[5]
+InitialSeed <- args[6]
+Npermutations <- args[7]
+
 ### Pick samples to drop
 
-CountTableChimpFile <- '../../output/PowerAnalysisFullCountTable.Chimp.subread.txt.gz'
-CountTableChimp <- read.table(gzfile(CountTableChimpFile), header=T, check.names=FALSE, skip=1)
-colnames(CountTableChimp) <- paste0("C.", colnames(CountTableChimp))
-
-CountTableHumanFile <- '../../output/PowerAnalysisFullCountTable.Human.subread.txt.gz'
-CountTableHuman <- read.table(gzfile(CountTableHumanFile), header=T, check.names=FALSE, skip=1)
-colnames(CountTableHuman) <- paste0("H.", colnames(CountTableHuman))
-
-CombinedTable <- inner_join(CountTableChimp[,c(1,7:length(CountTableChimp))], CountTableHuman[,c(1,7:length(CountTableHuman))], by=c("C.Geneid"="H.Geneid")) %>%
-  column_to_rownames("C.Geneid") %>% as.matrix()
-
-HumanSampleNames3 <- read.delim("../../output/PowerAnalysisCountTable.Human.SampleList.txt", col.names=c("SRA", "GTExName"), header=F, stringsAsFactors = F, sep=" ")
-
-HumanSamplesToDropOutOfCluster <- c("SRR613186", "SRR598509", "SRR1478149", "SRR603918", "SRR1507229")
-HumanSamplesToDropLowReads <- CombinedTable %>% colSums() %>%
-  as.data.frame() %>%
-  rownames_to_column("Sample") %>%
-  mutate(SampleNoSpecies = gsub("^[HC].", "", Sample)) %>%
-  inner_join(HumanSampleNames3, by=c("SampleNoSpecies"="SRA")) %>%
-  mutate(Species=substr(Sample, 1,1)) %>%
-  filter(Species=="H") %>%
-  filter(!SampleNoSpecies %in% HumanSamplesToDropOutOfCluster) %>%
-  top_n(-5, wt=as.numeric(`.`)) %>% pull(SampleNoSpecies)
-HumanSamplesToDrop <- c(HumanSamplesToDropOutOfCluster, HumanSamplesToDropLowReads)
-
+HumanSamplesToDrop <- c("SRR613186",  "SRR598509",  "SRR1478149", "SRR603918",  "SRR1507229", "SRR1478900", "SRR1477015", "SRR601986",  "SRR614996",  "SRR1474730")
 ChimpSamplesToDrop <- c()
+
+### Pick genes to analyze
+
+DE.results <- read.delim(OutputDE, sep='\t', stringsAsFactors = F)
+GeneListForOverdispersionCalculation <- DE.results$Ensembl_geneID
+
+NumRowsToAnalyze=length(GeneListForOverdispersionCalculation)
+NumRowsToAnalyze=100
+
+### Estimate overdispersion after random sampling with replacement of columns
+CountTables <- GetCountTables(CountTableChimpFile,
+                              CountTableHumanFile,
+                              0, GeneListForOverdispersionCalculation, ChimpSampleDrop=ChimpSamplesToDrop, HumanSampleDrop = HumanSamplesToDrop)
+
+
+HumanResultsMatrix <-matrix(data=NA, nrow=NumRowsToAnalyze, ncol=Npermutations)
+ChimpResultsMatrix <-matrix(data=NA, nrow=NumRowsToAnalyze, ncol=Npermutations)
+
+for (i in 1:Npermutations){
+  HumanResultsMatrix[,i] <- Resampled.GetDispersion(CountTables$Human$Counts,
+                                                    rep.col(CountTables$Human$GeneLengths, ncol(CountTables$Human$Counts)),
+                                                    seed=i,
+                                                    n=39,
+                                                    N_genes=NumRowsToAnalyze)
+  ChimpResultsMatrix[,i] <- Resampled.GetDispersion(CountTables$Chimp$Counts,
+                                                    rep.col(CountTables$Chimp$GeneLengths, ncol(CountTables$Chimp$Counts)),
+                                                    seed=i,
+                                                    n=39,
+                                                    N_genes=NumRowsToAnalyze)
+}
+
+row.names(ChimpResultsMatrix) <- rownames(CountTables$Chimp$Counts[1:NumRowsToAnalyze,])
+row.names(HumanResultsMatrix) <- rownames(CountTables$Human$Counts[1:NumRowsToAnalyze,])
+
+write.table(t(ChimpResultsMatrix), OutputDispersionEstimatesChimp, quote=F, sep='\t', col.names = T, row.names=F)
+write.table(t(HumanResultsMatrix), OutputDispersionEstimatesHuman, quote=F, sep='\t', col.names = T, row.names=F)
+
+
